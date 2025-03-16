a93b19fcc8d502024d381a1633528d3d7de9de56
ReinaldoJunior3108 <reinaldo310805@edu.unipar.br>
2025-03-15 22:53:56 -0300

    Portfolio

---
+++ main.js
@@ -0,0 +1,13 @@
+/*================== scroll reveal ========================*/
+ScrollReveal({
+  distance: "70px",
+  duration: 3000,
+  delay: 20,
+});
+
+ScrollReveal().reveal(".content-home, heading", { origin: "top" });
+ScrollReveal().reveal(".pfp, .container-servicos, .contact form", {
+  origin: "buttom",
+});
+ScrollReveal().reveal(".content-home h1, .education", { origin: "left" });
+ScrollReveal().reveal(".content-home p", { origin: "right" });
