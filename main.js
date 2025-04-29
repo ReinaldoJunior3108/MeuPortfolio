ff31979dfd4b65d10413033dde6b920d2a139b8b
reinaldojunior3108 <reinaldo310805@edu.unipar.br>
2025-04-29 15:40:02 -0300

    Substituido Serviços por Habilidades e Resumo

--- main.js
+++ main.js
@@ -14,3 +14,13 @@ ScrollReveal().reveal(".content-home p", { origin: "right" });
 ScrollReveal().reveal(".projetos", {
   origin: "top",
 });
+window.addEventListener("scroll", function () {
+  const btn = document.getElementById("actionBtn");
+  const scrollPoint = 800; // altura (em px) onde o botão vira fixo
+
+  if (window.scrollY > scrollPoint) {
+    btn.classList.add("fixed-btn");
+  } else {
+    btn.classList.remove("fixed-btn");
+  }
+});
